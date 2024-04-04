0df7a7e4026a8e7a3198c8d081fb86a8b5f2034c
Ludovic Roland <ludovic.c.roland@gmail.com>
2024-04-05 00:53:20 +0200

    chore: add kotlin and compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   alias(libs.plugins.androidApplication)
   alias(libs.plugins.hilt)
+  alias(libs.plugins.kotlin)
 }
 
 android {
@@ -27,21 +28,43 @@ android {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
   }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.5.11"
+  }
   buildFeatures {
+    compose = true
     viewBinding = true
   }
 }
 
 dependencies {
-
+  //to remove
   implementation(libs.appcompat)
   implementation(libs.material)
   implementation(libs.constraintlayout)
   implementation(libs.navigation.fragment)
   implementation(libs.navigation.ui)
+  implementation(libs.glide)
+
+  //kotlin
+  implementation(platform(libs.kotlin.bom))
+
+  //DI
   implementation(libs.hilt)
   annotationProcessor(libs.hilt.compiler)
-  implementation(libs.glide)
+
+  //compose
+  implementation(platform(libs.compose.bom))
+  implementation(libs.compose.ui)
+  implementation(libs.compose.ui.graphics)
+  implementation(libs.compose.ui.tooling.preview)
+  implementation(libs.material)
+  debugImplementation(libs.compose.ui.tooling)
+  debugImplementation(libs.compose.ui.test.manifest)
+
+  implementation(libs.activity.compose)
+  implementation(libs.navigation.compose)
+
   testImplementation(libs.junit)
   androidTestImplementation(libs.ext.junit)
   androidTestImplementation(libs.espresso.core)
